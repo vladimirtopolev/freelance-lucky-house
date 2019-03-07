d2e54dcf4e6e29b676c05b6f36cc87189323fd04
Vlad Topolev <uladzimir.topaleu@omnigon.com>
2019-03-07 11:36:22 +0300

    Add navigation

--- src/App.js
+++ src/App.js
@@ -19,24 +19,36 @@ class App extends Component {
                                     <div className="navigation__bar">
                                         <ul className="main-navbar">
                                             <li className="main-navbar__item">
-                                                <a href="#">О нас</a>
+                                                <a href="#" className="main-navbar__link">О нас</a>
                                             </li>
-                                            <li>
-                                                <a href="#">Проекты</a>
+                                            <li className="main-navbar__item">
+                                                <a href="#" className="main-navbar__link">Проекты</a>
                                             </li>
-                                            <li>
-                                                <a href="#">Отзывы</a>
+                                            <li className="main-navbar__item">
+                                                <a href="#" className="main-navbar__link">Отзывы</a>
                                             </li>
-                                            <li>
-                                                <a href="#">Контакты</a>
+                                            <li className="main-navbar__item">
+                                                <a href="#" className="main-navbar__link">Контакты</a>
                                             </li>
                                         </ul>
                                     </div>
 
                                     <div className="navigation__contact">
-                                        test
+                                        <div className="navbar-contact">
+                                            <div className="navbar-contact__label">
+                                                <div className="fa fa-phone navbar-contact__icon"></div>
+                                            </div>
+                                            <div className="navbar-contact__description">
+                                                <div className="navbar-contact__description-item">+7 911 404 60 22</div>
+                                                <div className="navbar-contact__description-item">nyppelli@mail.ru</div>
+                                            </div>
+                                        </div>
                                     </div>
                                 </div>
+                            </div>
+
+                            <div className="main-slider">
+
                             </div>
 
 

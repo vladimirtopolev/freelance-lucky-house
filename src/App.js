6b4a305203eb7b9e682bbfd88e0309b5eb188950
Vladimir Topolev <i.vladimir.topolev@gmail.com>
2019-03-07 08:23:32 +0300

    Init commit

--- src/App.js
+++ src/App.js
@@ -1,28 +1,53 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, {Component} from 'react';
+import './App.scss';
 
 class App extends Component {
-  render() {
-    return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
-      </div>
-    );
-  }
+    render() {
+        return (
+            <div className="wrapper">
+                <div className="content">
+                    <div className="main-view">
+                        <div className="main-view__header">
+
+
+                            <div className="navigation">
+                                <div className="navigation__container">
+                                    <div className="navigation__logo">
+                                        <img alt="logo" src={require('./img/logo.png')}/>
+                                    </div>
+
+                                    <div className="navigation__bar">
+                                        <ul className="main-navbar">
+                                            <li className="main-navbar__item">
+                                                <a href="#">О нас</a>
+                                            </li>
+                                            <li>
+                                                <a href="#">Проекты</a>
+                                            </li>
+                                            <li>
+                                                <a href="#">Отзывы</a>
+                                            </li>
+                                            <li>
+                                                <a href="#">Контакты</a>
+                                            </li>
+                                        </ul>
+                                    </div>
+
+                                    <div className="navigation__contact">
+                                        test
+                                    </div>
+                                </div>
+                            </div>
+
+
+                        </div>
+                    </div>
+                </div>
+                <div className="footer">
+                </div>
+            </div>
+        );
+    }
 }
 
 export default App;

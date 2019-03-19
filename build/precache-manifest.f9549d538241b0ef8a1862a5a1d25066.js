c29a18f1109480a2ad7e57af391d72366c6ddab1
vladimirtopolev <i.vladimir.topolev@gmail.com>
2019-03-19 22:12:34 +0300

    Prepare PROD launch on heroku

---
+++ build/precache-manifest.f9549d538241b0ef8a1862a5a1d25066.js
@@ -0,0 +1,86 @@
+self.__precacheManifest = [
+  {
+    "revision": "f2d1abe5a94d8a2ad5980cf648bdd87f",
+    "url": "/static/media/bg-portfolio.f2d1abe5.jpg"
+  },
+  {
+    "revision": "d05cfe882e5608a48b18",
+    "url": "/static/css/main.253808da.chunk.css"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.42ac5946.js"
+  },
+  {
+    "revision": "cd979431765bfdd83fd5290f8bfcfd60",
+    "url": "/static/media/photo5.cd979431.jpg"
+  },
+  {
+    "revision": "cb7252b45147e3362166",
+    "url": "/static/js/2.cb7252b4.chunk.js"
+  },
+  {
+    "revision": "bd62ffa16945bf73c8e90202f3d4982e",
+    "url": "/static/media/about-us-bg.bd62ffa1.png"
+  },
+  {
+    "revision": "eadd5967b460f99376d3580263c6e29a",
+    "url": "/static/media/slider-bg.eadd5967.jpg"
+  },
+  {
+    "revision": "b106d51e99f642a22bad540f886e017d",
+    "url": "/static/media/feedback.b106d51e.jpg"
+  },
+  {
+    "revision": "0383a250e987fe737edbf2518f208bc7",
+    "url": "/static/media/noimagefound.0383a250.jpg"
+  },
+  {
+    "revision": "9df30b1bdeb20a79c157d44cb1693285",
+    "url": "/static/media/no-user.9df30b1b.png"
+  },
+  {
+    "revision": "d05cfe882e5608a48b18",
+    "url": "/static/js/main.d05cfe88.chunk.js"
+  },
+  {
+    "revision": "ced611daf7709cc778da928fec876475",
+    "url": "/static/media/slick.ced611da.eot"
+  },
+  {
+    "revision": "b7c9e1e479de3b53f1e4e30ebac2403a",
+    "url": "/static/media/slick.b7c9e1e4.woff"
+  },
+  {
+    "revision": "d41f55a78e6f49a5512878df1737e58a",
+    "url": "/static/media/slick.d41f55a7.ttf"
+  },
+  {
+    "revision": "f97e3bbf73254b0112091d0192f17aec",
+    "url": "/static/media/slick.f97e3bbf.svg"
+  },
+  {
+    "revision": "edf52a77f17c316c84abc8ffb496be9d",
+    "url": "/static/media/photo1.edf52a77.jpg"
+  },
+  {
+    "revision": "96a5b61150570af4bd6ed2bbe3a645bd",
+    "url": "/static/media/photo2.96a5b611.jpg"
+  },
+  {
+    "revision": "9ef6620518c2cd52379c76f78c49f785",
+    "url": "/static/media/photo3.9ef66205.jpg"
+  },
+  {
+    "revision": "39dccfa91f722187d6c189e31da2e07d",
+    "url": "/static/media/photo4.39dccfa9.jpg"
+  },
+  {
+    "revision": "cb7252b45147e3362166",
+    "url": "/static/css/2.10f6d2f1.chunk.css"
+  },
+  {
+    "revision": "ececabbbc43a59b4ad812d9178ccb2d9",
+    "url": "/index.html"
+  }
+];
